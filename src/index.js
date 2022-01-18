dbef296f35ca7fb9993df58834cd67723a977b3b
Matt Draghi <mtd5264@gmail.com>
2022-01-17 22:28:00 -0500

    Added color changing functionality to priority selection

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   //add functionality to submit button
   document.querySelector(`form#create-task-form`).addEventListener('submit', (e) =>{
-    //prevent form submission from causing reset
     e.preventDefault()
     let newTask = e.target.querySelector(`input#new-task-description`).value
+    
     //add task to list
     if (newTask === '' || newTask === null){
       alert("Please enter an item")
     }
     else{
       addTask(newTask)
-      let newPriorityDropdown = document.querySelector(`ul#tasks li`).lastChild 
-      //addPriorityHandler(newPriorityDropdown)
+      let newPriorityDropdown = document.querySelector(`ul#tasks li`).lastChild
+      newPriorityDropdown.addEventListener('change', addPriorityHandler)
     }
     //reset the input field after task added
     document.querySelector(`form`).reset()
@@ -30,6 +30,7 @@ const addTask = task => {
   //create dropdown
   let dropdown = document.createElement(`select`)
   addPriorityOptions(dropdown)
+  
 
   //create list item with button and dropdown
   li.textContent = `${task} `
@@ -38,8 +39,6 @@ const addTask = task => {
 
   //add list item to list 
   document.querySelector(`div#list ul#tasks`).appendChild(li)
-
-
 }
 
 const deleteTask = e => {
@@ -47,23 +46,34 @@ const deleteTask = e => {
 }
 
 const addPriorityOptions = dropdown => {
-  const priorityOptions = ['High Priority', 'Medium Priority', 'Low Priority']
+  const priorityOptions = ['Select Priority', 'High Priority', 'Medium Priority', 'Low Priority']
+
   for (const priorityLevel in priorityOptions){
     let option = document.createElement(`option`)
     option.textContent = priorityOptions[priorityLevel]
     dropdown.add(option)
   }
+  //console.log(dropdown.querySelector('option'))
+  //dropdown.placeholder = "Select Priority"  //need to figure out how to add this as a placeholder
 }
 
-//change text color of item based on item priority selected
-// const addPriorityHandler = (dropdown) => {
-//   console.log(dropdown)
-// }
+const addPriorityHandler = e => {
+  const optionArray = []
+  const options = e.target.options
+  //console.log(options)
+  for(i = 0; i < options.length; i++){
+    optionArray.push(options[i])
+  }
 
-//add input field to allow user, duration, due date
-
-
-//add ability to edit tasks
-
-
-//add sorting functionality based on selected priority
+  let selectedPriority = e.target.value
+  let listItem = document.querySelector(`#tasks li`)
+  if(selectedPriority === "High Priority"){
+    listItem.style.color = "red"
+  }
+  else if(selectedPriority === "Medium Priority"){
+    listItem.style.color = "yellow"
+  }
+  else if(selectedPriority === "Low Priority"){
+    listItem.style.color = "green"
+  }
+}
